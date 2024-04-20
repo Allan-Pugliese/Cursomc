f7d0147b62eef97e962a6f4f31f9d2ed4eef4622
Allan Pugliese <allan_pugliesi@outlook.com>
2024-04-20 19:11:37 -0300

    Busca de pedidos por nome e Categorias pt2

--- src/main/java/com/allan/cursomc/repositores/ProdutoRepository.java
+++ src/main/java/com/allan/cursomc/repositores/ProdutoRepository.java
@@ -1,11 +1,24 @@
 package com.allan.cursomc.repositores;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.allan.cursomc.domain.Categoria;
 import com.allan.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	// @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat
+	// WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+
+	// O nome do metodo substitui a Query
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,
+			Pageable pageRequest);
 }
